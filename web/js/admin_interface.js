af4a8aa910f24f86e6757e45b62286e3abb56c2d
narmieno <jalenbuscemi@yahoo.de>
2020-03-03 08:35:12 +0100

    Admin interface modal input

--- web/js/admin_interface.js
+++ web/js/admin_interface.js
@@ -1,6 +1,18 @@
+const passwordInput = document.querySelector("#passwordInput")
+const userInput = document.querySelector("#userInput")
+const isAdminInput = document.querySelector("#isAdminInput")
+const confirmUserAdd = document.querySelector("#confirmUserAdd")
 
+function addUser() {
+    const data = {
+        userName: userInput.value,
+        password: passwordInput.value,
+        administrator: isAdminInput.checked,
+    };
+    console.log(data);
+}
 
 
 $(window).on("load", function () {
-    
+    confirmUserAdd.addEventListener("click", addUser);
 })
