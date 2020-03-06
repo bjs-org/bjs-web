fa841c8ef1b1489116e5fd69b3abbd0c1952d8ed
narmieno <jalenbuscemi@yahoo.de>
2020-03-06 12:10:54 +0100

    Show password final

--- web/js/admin_interface.js
+++ web/js/admin_interface.js
@@ -2,6 +2,7 @@ const passwordInput = document.querySelector("#passwordInput")
 const userInput = document.querySelector("#userInput")
 const isAdminInput = document.querySelector("#isAdminInput")
 const confirmUserAdd = document.querySelector("#confirmUserAdd")
+const showPassword = document.querySelector("#showPassword")
 
 function addUser() {
     const data = {
@@ -13,6 +14,16 @@ function addUser() {
 }
 
 
-$(window).on("load", function () {
-    confirmUserAdd.addEventListener("click", addUser);
-})
+
+confirmUserAdd.addEventListener("click", addUser);
+showPassword.onclick = () => {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        const icon = showPassword.querySelector("a > i");
+        icon.classList.replace("fa-eye","fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        const icon = showPassword.querySelector("a > i");
+        icon.classList.replace("fa-eye-slash","fa-eye");
+    }
+}
